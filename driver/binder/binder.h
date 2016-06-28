f7b549f9a266709736bfcc1ea997a5b771445cc6
Yu-Hsin Hung <hungys@hotmail.com>
2016-06-28 12:34:23 +0800

    Fix ioctl error for binder_handle_cookie struct (packed attribute)

--- driver/binder/binder.h
+++ driver/binder/binder.h
@@ -170,8 +170,7 @@ struct binder_ptr_cookie {
 struct binder_handle_cookie {
 	__u32 handle;
 	binder_uintptr_t cookie;
-};
-// } __packed;
+} __attribute__((packed));
 
 struct binder_pri_desc {
 	__s32 priority;
